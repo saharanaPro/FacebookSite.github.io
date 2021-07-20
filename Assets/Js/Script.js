433a07fedcd489177757ef8bd309233057143a61
saharanaPro <saharana.offical8@gmail.com>
2021-07-20 20:49:14 +0530

    New site facebook

---
+++ Assets/Js/Script.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+'use strict';
+
+document.getElementById("hide").onclick = function () {
+
+    document.getElementById("overlay").style.display = 'none';
+
+}
+
+document.getElementById("show").onclick = function () {
+
+    document.getElementById("overlay").style.display = "block";
+
+}
